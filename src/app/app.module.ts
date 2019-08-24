89897434936111268123bf1e64ba628643e06210
alexandrurdev <contact@ardev.ro>
2019-08-24 21:43:43 +0300

    passing data on a material dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatIconModule, MatButtonModule, MatChipsModule, MatProgressSpinnerModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { EditCourseComponent } from './edit-course/edit-course.component';
+import { CourseService } from './course.service';
+import { MatComponentsModule } from './mat-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditCourseComponent
+  ],
+  entryComponents: [
+    EditCourseComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatButtonModule,
-    MatChipsModule,
-    MatProgressSpinnerModule
+    MatComponentsModule
   ],
-  providers: [],
+  providers: [
+    CourseService,
+    { provide: CourseService, useClass: CourseService },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
